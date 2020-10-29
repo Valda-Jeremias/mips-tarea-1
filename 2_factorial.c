8510e9712e747f1a7feffbdbb2f1bc9b77a449f9
Santiago Trini <santiagotrini@gmail.com>
2020-10-29 01:30:55 -0300

    ejercicio 2 terminado

--- 2_factorial.c
+++ 2_factorial.c
@@ -1,7 +1,16 @@
+// factorial.c
+// calcula el factorial de un numero ingresado por el usuario
 #include <stdio.h>
 
 int main() {
+  int n = 0;
+  printf("Ingrese un numero: ");
+  scanf("%d", &n);
   int res = 1;
-  if ()
+  while (n > 0) {
+    res *= n;
+    n--;
+  }
+  printf("%d\n", res);
   return 0;
 }
